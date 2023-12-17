02935507b57798d23cbc36344693c5ed7a2be42e
Douglas-Skimp <douglas.rodrigues.a@gmail.com>
2023-12-17 07:33:14 -0300

    First commit

--- www/ignite/nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ www/ignite/nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,13 +1,11 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
+
 import { json } from './middleware/json.js'
 import { routes } from './routes.js'
 
 // UUID => Unique Universal ID
 
 
-const database = new Database
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -17,6 +15,8 @@ const server = http.createServer(async (req, res) => {
     const routes = routes.find(route => {
       return route.method === method && route.path === url 
     })
+
+    console.log(route)
   
     return res.writeHead(404).end()
   })
