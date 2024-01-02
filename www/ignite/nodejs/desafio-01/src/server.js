b046766fe4477426b7bcd9c2698e120582866d1c
Douglas-Skimp <douglas.rodrigues.a@gmail.com>
2024-01-02 14:31:36 -0300

    Primeiro desafio nodejs

--- www/ignite/nodejs/desafio-01/src/server.js
+++ www/ignite/nodejs/desafio-01/src/server.js
@@ -1,6 +1,6 @@
 import http from 'node:http'
 
-import { json } from './middlewares/json.js'
+import { json } from './middleware/json.js'
 import { routes } from './routes.js'
 import { extractQueryParams } from './utils/extract-query-params.js'
 
@@ -27,4 +27,4 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(4343)
+server.listen(3333)
